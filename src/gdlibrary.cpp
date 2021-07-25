9d1ac440acb00c664c6b3cb1858b2053aec903b3
raznan <muhammadraznan@satpaminternet.com>
2021-07-25 13:41:44 +0700

    Added src files

---
+++ src/gdlibrary.cpp
@@ -0,0 +1,16 @@
+#include "gdml.h"
+
+extern "C" {
+  void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *options) {
+    godot::Godot::gdnative_init(options);
+  }
+
+  void GDN_EXPORT godot_gdnative_terminate(godot_gdnative_terminate_options *options) {
+    godot::Godot::gdnative_terminate(options);
+  }
+
+  void GDN_EXPORT godot_nativescript_init(void *handle) {
+    godot::Godot::nativescript_init(handle);
+    godot::register_class<godot::GDML>();
+  }
+};
--- src/gdml.h
+++ src/gdml.h
@@ -1,10 +1,22 @@
 #ifndef GDML_H
 #define GDML_H
 
+#include <Godot.hpp>
+#include <Node.hpp>
+
+
 namespace godot {
-  class GDML : public Reference {
-
-
+  class GDML : public Node {
+    GODOT_CLASS(GDML, Node)
+  
+  private:
+  public:
+    static void _register_methods();
+    GDML();
+    ~GDML();
+    void test();
+    void _ready();
+    void _init();
   };
 }
 
